82e005552dc79f8985e4d9b7cee935339ea56512
jrichocean <jrichocean@gmail.com>
2016-05-04 16:56:01 -0600

    ADDS 'caffiene' app to menubar

--- init.lua
+++ init.lua
@@ -14,3 +14,25 @@ end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config Reloaded")
 
+
+
+-- Caffeine
+-- 
+local caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+    if state then
+        caffeine:setTitle("C₈H₁₀N₄O₂")
+    else
+        caffeine:setTitle("Sleepy")
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
